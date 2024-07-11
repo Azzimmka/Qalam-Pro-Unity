ea783b5ecb90459970b248e3720a0814ab26631b
azim gulyamov <azimgulyamov@192.168.0.176>
2024-07-10 22:26:01 -0700

    2 lesson create models Post and superuser

--- app/models.py
+++ app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
+
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(unique=True)
+    image = models.ImageField()
+    description = models.TextField()
+    date_pub = models.DateTimeField(default=timezone.now)
+
 
 # Create your models here.
