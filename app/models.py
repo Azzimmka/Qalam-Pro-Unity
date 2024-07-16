cfc38a19d0765247c0749adad5295f7c7cfd9b6b
azim gulyamov <azimgulyamov@192.168.1.9>
2024-07-16 00:10:58 -0700

    commit 4 add models Post and Category

--- app/models.py
+++ app/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.utils import timezone
+from django.urls import reverse
 
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.title
 
 
 class Post(models.Model):
@@ -9,6 +16,7 @@ class Post(models.Model):
     image = models.ImageField()
     description = models.TextField()
     date_pub = models.DateTimeField(default=timezone.now)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
 
 
     def __str__(self):
--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post, Category
 # Create your views here.
 def index(request):
-    post = Post.objects.all()
-    return render(request, 'index.html', {"post": post})
+    post = Post.objects.order_by('date_pub')[:1]
+    category = Category.objects.all()
+    return render(request, 'index.html', {"post": post, 'category': category})
